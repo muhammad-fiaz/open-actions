8f81b7ed342f2af814ced32776a139d460feef3a
Muhammad Fiaz <contact@muhammadfiaz.com>
2024-08-13 21:48:32 +0530

    updated configs improved

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// index.ts
+
+export * from './ajax';// Export AJAX utilities
+export * from './dom';// Export DOM utilities
+export * from './events';// Export event handling utilities
+export * from './css';        // Export CSS utilities
+export * from './animation';  // Export animation utilities
+export * from './select';     // Export select utilities
+export * from './form'; // Add new form handling functionality
+export * from './storage'; // Add new localStorage functionality
+export * from './eventDelegation'; // Export event delegation utilities
+export * from './utils'; // Export utility functions
